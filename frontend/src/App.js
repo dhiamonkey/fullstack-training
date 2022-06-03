2dcb788771ee75ea4c741f4035e4bfd7762dd32f
dhia muhammad <dhiamuhammad@gmail.com>
2022-06-03 17:33:23 +0700

    frontend init

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold text-blue">
+        lorem ipsum doloret si amet
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/UserList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserList = () => {
+  return <div className="">User List</div>;
+};
+
+export default UserList;
