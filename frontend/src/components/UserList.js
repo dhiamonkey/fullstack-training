cd7e29b595faceeebb1a377d6292192d93ef5ec4
dhia muhammad <dhiamuhammad@gmail.com>
2022-06-03 18:34:03 +0700

    fixing my tailwind intellisense

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const UserList = () => {
-  return <div className="">User List</div>;
+  return <div className="columns-1">User List</div>;
 };
 
 export default UserList;
